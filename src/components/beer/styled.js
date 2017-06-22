3e7e960b00f9689d44f4ed98ada1d14a49872e6d
Kylor Hall <kylor@kylorhall.com>
2017-06-22 16:57:57 +1200

    Make sure buttons stay circular in all circumstances – migrating some parent margin to child.

--- src/components/beer/styled.js
+++ src/components/beer/styled.js
@@ -57,7 +57,7 @@ export const Stats = styled(Flex).attrs({
   align: 'center',
   justify: 'center'
 })`
-  margin: 24px 0;
+  margin: 16px 0;
 `
 
 export const Stat = styled(Box).attrs({
@@ -73,7 +73,7 @@ export const Stat = styled(Box).attrs({
 
   width: 60px;
   height: 60px;
-  margin: 0 5px;
+  margin: 5px;
 
   header {
     display: block;
